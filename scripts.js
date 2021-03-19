b57ae609d3d0b46b62907be9bd101bc629432e4b
MayaraPimenta <mayaralrpimenta@gmail.com>
2021-03-18 21:28:21 -0300

    Js updated

--- scripts.js
+++ scripts.js
@@ -3,16 +3,28 @@ let task = document.getElementById("task")
 let newInput = document.getElementById("newInput")
 let newTask = document.getElementById("newTask")
 
-function exclude() {
-    //task.remove();
-}
-
-function duplicate() {
-    var newLi = document.createElement("li")
+function duplicate() {       
+    let newLi = document.createElement("li")
     newLi.setAttribute("id", "newTask")
-    list.appendChild(newLi);
+    list.appendChild(newLi)
+    
+    let check = document.createElement("input")
+    check.setAttribute("type", "checkbox")
+    newLi.appendChild(check)
+    
+    let label = document.createElement("label")
+    newLi.appendChild(label)
 
-    newLi.textContent = newInput.value;
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7")
+    span.appendChild(txt)
+    span.onclick = function() {
+        list.removeChild(this.parentNode)
+    }
+    newLi.appendChild(span)
 }
 
+function checked () {
+    
+}
 
